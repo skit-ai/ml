aa50c23d568a7432ce82238aee785e8ed52567af
Abhinav Tushar <abhinav@lepisma.xyz>
2020-04-19 15:16:16 +0530

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,30 +4,33 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitle: `Machine Learning at Vernacular.ai`,
+    siteTitleAlt: `Machine Learning | Vernacular.ai`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       options: {
+        feed: true,
+        feedTitle: `Machine Learning at Vernacular.ai`,
         navigation: [
           {
             title: `Blog`,
             slug: `/blog`,
           },
           {
+            title: `Team`,
+            slug: `/team`,
+          },
+          {
             title: `About`,
             slug: `/about`,
-          },
+          }
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `github`,
+            url: `https://github.com/Vernacular-ai`,
           },
         ],
       },
@@ -42,9 +45,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Machine Learning at Vernacular.ai`,
+        short_name: `machine learning`,
+        description: `Notes from the Machine Learning team at Vernacular.ai`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
